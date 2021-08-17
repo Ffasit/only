6f7b0adfc8454ddf58015444c47fd6463b8a555c
Ffasit <85315003+Ffasit@users.noreply.github.com>
2021-08-17 05:47:57 +0200

    Add files via upload

--- BANDA.lua
+++ BANDA.lua
@@ -4747,7 +4747,17 @@ end
 end
 
 
-
+if text == "$-+)(&@٠١" and not  database:get(bot_id.."sing:for:me"..msg.chat_id_) then 
+ght = math.random(3,200); 
+local Text ='تم اختيار المقطع الصوتي لك' 
+keyboard = {}  
+keyboard.inline_keyboard = { 
+{{text = 'اضف البوت الي مجموعتك' ,url="t.me/"..dofile("./kkkklInfo.lua").botUserName.."?startgroup=start"}}, 
+} 
+local msg_id = msg.id_/2097152/0.5 
+https.request("https://api.telegram.org/bot"..token..'/sendVoice?chat_id=' .. msg.chat_id_ .. '&voice=https://t.me/Ccckkc/'..ght..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+end
+end
 if text == 'غنيلي' then 
 Num = math.random(8,83)
 Mhm = math.random(108,143)
