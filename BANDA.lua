59646ba0508534b28aa8d1d356a45088c070cdd9
Ffasit <85315003+Ffasit@users.noreply.github.com>
2021-08-21 05:41:34 +0200

    Add files via upload

--- BANDA.lua
+++ BANDA.lua
@@ -164,7 +164,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/Bruc468/nasa/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/Ffasit/only/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -1810,7 +1810,7 @@ echo '⩹━━━━◍『ᎬᏙᎪᏞᏞ』◍━━━━⩺\n ◍「 مـد�
 end
 if text == 'تحديث السورس ' and Debn(msg) then 
 os.execute('rm -rf BANDA.lua')
-os.execute('wget https://raw.githubusercontent.com/Bruc468/nasa/main/BANDA.lua')
+os.execute('wget https://raw.githubusercontent.com/Ffasit/only/main/BANDA.lua')
 send(msg.chat_id_, msg.id_,' ◍ تم تحديث السورس')
 dofile('BANDA.lua')  
 end
@@ -3442,7 +3442,7 @@ end
 return false
 end
 os.execute('rm -rf BANDA.lua')
-os.execute('wget https://raw.githubusercontent.com/Bruc468/nasa/main/BANDA.lua')
+os.execute('wget https://raw.githubusercontent.com/Ffasit/only/main/BANDA.lua')
 send(msg.chat_id_, msg.id_,' ◍ تم تحديث السورس')
 dofile('BANDA.lua')  
 end
@@ -6389,7 +6389,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if Debn(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/Bruc468/nasa/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/Ffasit/only/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -6427,7 +6427,7 @@ t = " ◍ الملف  ⇠ "..file.."\n ◍ تم تعطيل ملف \n"
 else
 t = " ◍ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/Bruc468/nasa/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/Ffasit/only/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -6447,7 +6447,7 @@ t = " ◍ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ◍ الملف  ⇠ "..file.."\n ◍ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/Bruc468/nasa/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/Ffasit/only/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
